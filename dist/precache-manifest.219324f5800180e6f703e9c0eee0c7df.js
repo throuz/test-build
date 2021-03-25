43592838d16276289d00183afa8d9d905dec430c
throuz <64568129+throuz@users.noreply.github.com>
2021-03-25 23:33:07 +0800

    build

---
+++ dist/precache-manifest.219324f5800180e6f703e9c0eee0c7df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a74a82c715ca128b19a1",
+    "url": "/css/app.f8b9661e.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "6e473860844a83990bd49086a467d689",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3436ea3ac82482f3e3a0",
+    "url": "/js/about.063687bd.js"
+  },
+  {
+    "revision": "a74a82c715ca128b19a1",
+    "url": "/js/app.14b3d346.js"
+  },
+  {
+    "revision": "a4faa6543e29fe97812f",
+    "url": "/js/chunk-vendors.d49598d0.js"
+  },
+  {
+    "revision": "4d95030b488cf00f593220a3ffc9c5f0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
